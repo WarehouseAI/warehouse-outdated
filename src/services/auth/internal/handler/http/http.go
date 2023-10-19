dbee1627084387c4a90d179154aa42fa1f5d7db6
Sergey Dyakov <s.dyakov9@gmail.com>
2023-10-19 16:57:55 +0300

    fix cookies

--- src/services/auth/internal/handler/http/http.go
+++ src/services/auth/internal/handler/http/http.go
@@ -80,8 +80,9 @@ func (pvd *AuthServiceProvider) LoginHandler(c *fiber.Ctx) error {
 	}
 
 	c.Cookie(&fiber.Cookie{
-		Name:  "sessionId",
-		Value: session.ID,
+		Name:     "sessionId",
+		Value:    session.ID,
+		SameSite: fiber.CookieSameSiteNoneMode,
 	})
 
 	return c.SendStatus(fiber.StatusOK)
